a74b108a54f72dfe5782564962cd7feff7c7f150
zhangtian <zhangtian@zhuohan-tech.com>
2018-08-30 11:30:46 +0800

    添加二分搜索树删除逻辑

--- src/com/datastructure/BST.java
+++ src/com/datastructure/BST.java
@@ -1,5 +1,9 @@
 package com.datastructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BST<E extends Comparable<E>> {
 
     private int size;
@@ -96,6 +100,41 @@ public class BST<E extends Comparable<E>> {
         }
     }
 
+    // 二分搜索树的前序遍历(非递归写法)(深度优先遍历)
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            //栈后进先出 故先入右节点
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
+    //二分搜索树的层序遍历(广度优先遍历)
+    public void levelOrder() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node cur = q.remove();
+            System.out.println(cur.e);
+
+            if (cur.left != null) {
+                q.add(cur.left);
+            }
+            if (cur.right != null) {
+                q.add(cur.right);
+            }
+        }
+    }
+
     // 二分搜索树的前序遍历
     public void preOrder() {
         preOrder(root);
@@ -126,6 +165,64 @@ public class BST<E extends Comparable<E>> {
         inOrder(node.right);
     }
 
+    // 寻找二分搜索树的最小元素(非递归写法)
+    public E minimumNR() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        Node cur = root;
+        while (cur.left != null) {
+            cur = cur.left;
+        }
+        return cur.e;
+    }
+
+    // 寻找二分搜索树的最大元素(非递归写法)
+    public E maximumNR() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        Node cur = root;
+        while (cur.right != null) {
+            cur = cur.right;
+        }
+        return cur.e;
+    }
+
+    // 寻找二分搜索树的最小元素(递归写法)
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return minimum(root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+
+        return minimum(node.left);
+    }
+
+    // 寻找二分搜索树的最大元素（递归写法）
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return maximum(root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最大值所在的节点
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+
+        return maximum(node.right);
+    }
+
     public void postOrder() {
         postOrder(root);
     }
@@ -141,6 +238,96 @@ public class BST<E extends Comparable<E>> {
         postOrder(node.right);
     }
 
+    //从二分搜索树中删除最小值所在的节点，返回最小值
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    //删除以node为根的二分搜索树中的最小节点
+    //返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    //从二分搜索树中删除最da值所在的节点，返回最大值
+    public E removeMax() {
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    //删除以node为根的二分搜索树中的最大节点
+    //返回删除节点后新的二分搜索树的根
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    // 从二分搜索树中删除元素为e的节点
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    //删除以node为根的二分搜索树中值为e节点，递归算法
+    //返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, E e) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else { //e == node.e
+
+            //待删除节点左子树为空
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            //待删除节点右子树为空
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            //待删除节点左右子树均不为空的情况
+            //找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+    }
 
     @Override
     public String toString() {
--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -2,6 +2,10 @@ package com.test;
 
 import com.datastructure.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -93,21 +97,48 @@ public class Main {
 //            }
 //        }
 
+//        BST<Integer> bst = new BST<>();
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+//        ///////////////////
+//        //       5       //
+//        //     /  \      //
+//        //    3   6      //
+//        //   / \   \     //
+//        //  2  4   8     //
+//        ///////////////////
+//        for (int num : nums) {
+//            bst.add(num);
+//        }
+//        bst.preOrder();
+//        bst.preOrderNR();
+//        bst.inOrder();
+//        bst.postOrder();
+//        bst.preOrderNR();
+//        bst.levelOrder();
+
+
         BST<Integer> bst = new BST<>();
-        int[] nums = {5, 3, 6, 8, 4, 2};
-        ///////////////////
-        //       5       //
-        //     /  \      //
-        //    3   6      //
-        //   / \   \     //
-        //  2  4   8     //
-        ///////////////////
-        for (int num : nums) {
-            bst.add(num);
+        Random random = new Random();
+
+        int n = 1000;
+
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
         }
-        bst.preOrder();
-        bst.inOrder();
-        bst.postOrder();
-        System.out.println(bst.toString());
+
+        List<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            nums.add(bst.removeMin());
+        }
+        System.out.println(nums);
+
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) > nums.get(i)) {
+                throw new IllegalArgumentException("error");
+            }
+        }
+
+        System.out.println("removeMin success!");
     }
 }
