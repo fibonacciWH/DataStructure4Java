60efd21a6ccf83ec01053506d4179f13f04e1d64
zhangtian <zhangtian@zhuohan-tech.com>
2018-08-31 16:29:01 +0800

    添加基于链表和二分搜索树的映射

---
+++ src/com/datastructure/MapInterface.java
@@ -0,0 +1,17 @@
+package com.datastructure;
+
+public interface MapInterface<K, V> {
+    void add(K key, V value);
+
+    V remove(K key);
+
+    boolean contains(K key);
+
+    V get(K key);
+
+    void set(K key, V newValue);
+
+    int getSize();
+
+    boolean isEmpty();
+}
